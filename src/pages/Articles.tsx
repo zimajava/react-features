b61bad4148671c5aa00dd2932619908b6f7acfda
Zima Maksim <maksimz@wizardsdev.com>
2022-04-19 12:37:56 +0300

    feat: slate example added

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,5 +1,75 @@
 import React from 'react';
+import { createEditor, Descendant, Editor, Transforms } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+];
+
+type PropsCodeElement = {
+  attributes: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLPreElement> & React.HTMLAttributes<HTMLPreElement>;
+  children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined;
+};
+
+function CodeElement(props: PropsCodeElement) {
+  const { attributes, children } = props;
+
+  return (
+    <pre {...attributes}>
+      <code>{children}</code>
+    </pre>
+  );
+}
+
+type PropsDefaultElement = {
+  attributes: JSX.IntrinsicAttributes &
+    React.ClassAttributes<HTMLParagraphElement> &
+    React.HTMLAttributes<HTMLParagraphElement>;
+  children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined;
+};
+
+function DefaultElement(props: PropsDefaultElement) {
+  const { attributes, children } = props;
+
+  return <p {...attributes}>{children}</p>;
+}
 
 export default function Articles() {
-  return <h2>Articles</h2>;
+  const editor = React.useMemo(() => withReact(createEditor()), []);
+
+  const renderElement = React.useCallback((props) => {
+    const { attributes, children } = props;
+    // eslint-disable-next-line sonarjs/no-small-switch
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement attributes={attributes}>{children}</CodeElement>;
+      default:
+        return <DefaultElement attributes={attributes}>{children}</DefaultElement>;
+    }
+  }, []);
+
+  return (
+    <Slate editor={editor} value={initialValue}>
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === '`' && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault();
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, { match: (n) => n.type === 'code' });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              { match: (n) => Editor.isBlock(editor, n) },
+            );
+          }
+        }}
+      />
+    </Slate>
+  );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,53 @@
-/// <reference types="react-scripts" />
+/// <reference types="node" />
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="react" />
+/// <reference types="react-dom" />
+import { BaseEditor, Descendant } from 'slate';
+import { ReactEditor } from 'slate-react';
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly PUBLIC_URL: string;
+  }
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+  const src: string;
+  export default src;
+}
